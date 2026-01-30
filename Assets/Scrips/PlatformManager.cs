79bdd63c23e0a33a0ad8797b77485a953a229ed7
nestorzac <nestorzacarias0211@hotmail.com>
2026-01-29 19:52:38 -0600

    PlatformPrefabs

---
+++ Assets/Scrips/PlatformManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlataformManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform platformsPivot;
+    [SerializeField]
+    private GameObject[] platformPrefabs;
+    [SerializeField] 
+    private int initialPlatforms = 5;
+    [SerializeField]
+    private float speed = 5f;
+    private bool isRunning = true;
+    private GameObject lastPlatform;
+
+    private void Start()
+    {
+        InstantiatePlatforms(initialPlatforms);
+        transform.position = platformsPivot.position;
+    }
+
+    private void InstantiatePlatforms(int number)
+    {
+        for (int i = 0; i < number; i++)
+        {
+            GameObject platformPrefab = platformPrefabs[Random.Range(0, platformPrefabs.Length)];
+            Vector3 spawnPosition = Vector3.zero;
+            if (lastPlatform != null)
+            {
+                spawnPosition = lastPlatform.transform.localPosition + lastPlatform.GetComponent<Collider>().bounds.size.z * Vector3.forward * 0.5f;
+
+            }
+            GameObject newPlatform =Instantiate(platformPrefab, Vector3.zero, Quaternion.identity, transform);
+            newPlatform.transform.localPosition = spawnPosition + newPlatform.GetComponent<Collider>().bounds.size.z * Vector3.forward * 0.5f;
+            lastPlatform = newPlatform; 
+        }
+    }
+    private void Update()
+    {
+        if (isRunning)
+        {
+            platformsPivot.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+    }
+
+}
