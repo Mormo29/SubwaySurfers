101f95f023923493dd94fceb2b80285797dc28c1
nestorzac <nestorzacarias0211@hotmail.com>
2026-02-04 20:38:03 -0600

    PoolPlatforms

--- Assets/Scrips/Character.cs
+++ Assets/Scrips/Character.cs
@@ -18,10 +18,18 @@ public class Character : MonoBehaviour
     private bool isGrounded = true;
     private bool isMoving = false;
     private bool isRolling = false;
+    private bool isActive = false;
     private void Start()
     {
+        isActive = true;
         characterAnimator.Play(characterData.runAnimationName, 0, 0f);
         characterRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Lose()
+    {
+        StopAllCoroutines();
+        characterAnimator.Play(characterData.loseAnimationName, 0, 0f);
     }
     public void Jump()
     {
@@ -73,7 +81,7 @@ public class Character : MonoBehaviour
     }
     public void OnCollisionEnter (Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (isActive && collision.gameObject.CompareTag("Ground"))
         {
             if (!isRolling)
             {
--- Assets/Scrips/PlatformManager.cs
+++ Assets/Scrips/PlatformManager.cs
@@ -5,7 +5,7 @@ public class PlataformManager : MonoBehaviour
     [SerializeField]
     private Transform platformsPivot;
     [SerializeField]
-    private GameObject[] platformPrefabs;
+    private InstantiatePoolObjects[] platformPrefabs;
     [SerializeField] 
     private int initialPlatforms = 5;
     [SerializeField]
@@ -19,28 +19,35 @@ public class PlataformManager : MonoBehaviour
         transform.position = platformsPivot.position;
     }
 
-    private void InstantiatePlatforms(int number)
+    public void InstantiatePlatforms(int number)
     {
         for (int i = 0; i < number; i++)
         {
-            GameObject platformPrefab = platformPrefabs[Random.Range(0, platformPrefabs.Length)];
+            InstantiatePoolObjects instantiatePool = platformPrefabs[Random.Range(0, platformPrefabs.Length)];
             Vector3 spawnPosition = Vector3.zero;
             if (lastPlatform != null)
             {
                 spawnPosition = lastPlatform.transform.localPosition + lastPlatform.GetComponent<Collider>().bounds.size.z * Vector3.forward * 0.5f;
 
             }
-            GameObject newPlatform =Instantiate(platformPrefab, Vector3.zero, Quaternion.identity, transform);
+            instantiatePool.InstantiateObject(spawnPosition);
+            GameObject newPlatform = instantiatePool.GetCurrentObject();
+            newPlatform.transform.SetParent(transform);
             newPlatform.transform.localPosition = spawnPosition + newPlatform.GetComponent<Collider>().bounds.size.z * Vector3.forward * 0.5f;
-            lastPlatform = newPlatform; 
+            lastPlatform = newPlatform;  
         }
     }
     private void Update()
     {
         if (isRunning)
         {
-            platformsPivot.Translate(Vector3.back * speed * Time.deltaTime);
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
     }
+    public void StopPlatforms()
+    {
+        isRunning = false;
+    }
+
 
 }
